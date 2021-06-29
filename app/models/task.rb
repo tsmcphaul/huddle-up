be42130997410129f9d6978e6bb482cbb61e5c92
Tammarra McPhaul <shaemc22@gmail.com>
2021-06-29 00:25:56 -0400

    added routes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
     belongs_to :user
-    belongs_to :Project
+    belongs_to :project
 
     validates_presence_of :name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+
+  root 'sessions#home'
+  
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   resources :tasks
-  resources :projects
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :projects do 
+    resources :tasks, only: [:new]
+  end
+  resources :users, only: [:new, :create] do
+    resources :tasks, only: [:index, :show]
+  end
+
+
 end
